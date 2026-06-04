172ee3f61baf5d8756e297a9794efc0a05a2a047
Rikvalix <titouan.delion.desrochers@outlook.fr>
2026-06-04 18:57:29 +0200

    feat: comment interfaces of repo and storageService

--- src/main/kotlin/com/transfair/domain/ports/FileRepository.kt
+++ src/main/kotlin/com/transfair/domain/ports/FileRepository.kt
@@ -1,15 +1,27 @@
 package com.transfair.domain.ports
 
 import com.transfair.domain.models.FileMetadata
-import java.util.UUID
+import java.util.*
 
 interface FileRepository {
 
-    suspend fun saveFile(fileMetadata: FileMetadata) : FileMetadata
+    /**
+     * @param fileMetadata metadata of the file to be saved
+     */
+    suspend fun saveFile(fileMetadata: FileMetadata): FileMetadata
 
-    suspend fun findById(uuid: UUID) : FileMetadata?
+    /**
+     * @param uuid unique identifier of the file
+     */
+    suspend fun findById(uuid: UUID): FileMetadata?
 
-    suspend fun deleteFile(uuid: UUID) : Boolean
+    /**
+     * @param uuid unique identifier of the file
+     */
+    suspend fun deleteFile(uuid: UUID): Boolean
 
-    suspend fun incrementDownloads(uuid: UUID) : Boolean
+    /**
+     * @param uuid unique identifier of the file
+     */
+    suspend fun incrementDownloads(uuid: UUID): Boolean
 }
--- src/main/kotlin/com/transfair/domain/ports/StorageService.kt
+++ src/main/kotlin/com/transfair/domain/ports/StorageService.kt
@@ -1,14 +1,26 @@
 package com.transfair.domain.ports
 
-import java.io.File
 import java.io.InputStream
 import java.util.*
 
 interface StorageService {
 
+    /**
+     * @param fileId unique identifier of the file
+     * @param content content of the file
+     * @return true if the file was saved successfully, false otherwise
+     */
     suspend fun saveFile(fileId: UUID, content: InputStream): Boolean
 
+    /**
+     * @param fileId unique identifier of the file
+     * @return the file content if it exists, null otherwise
+     */
     suspend fun getFile(fileId: UUID): InputStream?
 
+    /**
+     * @param fileId unique identifier of the file
+     * @return true if the file was deleted successfully, false otherwise
+     */
     suspend fun deleteFile(fileId: UUID): Boolean
 }
